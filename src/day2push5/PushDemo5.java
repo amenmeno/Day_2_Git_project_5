bf2f257bb99d485e44e6d887cc4769902495e5d1
meno <meno@menos-mbp.hsd1.mi.comcast.net>
2023-04-10 12:57:05 -0400

    test5

--- src/day2push5/PushDemo5.java
+++ src/day2push5/PushDemo5.java
@@ -4,4 +4,7 @@ public class PushDemo5 {
 		public static void main(String[] args) {
 				System.out.println("THIS IS PROJ 5");
 		}
+		public static void test() {
+			System.out.println("THIS IS TEST");
+		}
 }
